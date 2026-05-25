33a080c36361ef96613e72adba60ec33c08c3dc5
OlalSam <olalsamuel01@gmail.com>
2026-05-25 11:45:04 +0300

    Max age

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -3,7 +3,7 @@ import authConfig from "./auth.config"
 
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
-  session: { strategy: "jwt", maxAge: 2 * 59 * 60 },
+  session: { strategy: "jwt", maxAge: 24 * 60 * 60 },
   pages: {
     signIn: '/dashboard',   
     signOut: '/', 
